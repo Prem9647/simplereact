8a9da3ea9f4d5d2ffb9b575023702cd8c0f4274a
Prem9647 <itzprem2006@gmail.com>
2026-05-07 13:49:50 +0530

    first commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,10 @@
+import logo from "../assets/logo.png";
+
 function Navbar() {
   return (
     <nav className="navbar">
 
-      <img style={{ width: "150px" }}  src="src/assets/logo.png" alt="logo" />
+      <img style={{ width: "150px" }}  src={logo} alt="logo" />
 
       <ul>
         <li>Home</li>
